e28641b082abc763a6d1eae6fcc4adc7f7eb5dc3
bluephoenix42 <kohlmoja@gmx.de>
2016-07-24 17:58:23 +0200

    core

---
+++ core/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jul 24 16:52:57 2016
+
+@author: Jan
+"""
+
+import core.field
+import core.tower 
+import core.attacker
---
+++ game.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jul 24 16:52:00 2016
+
+@author: Jan
+"""
+
