2c4f86b3a4c1e4d27696ceb431b7155e707d90f7
bluephoenix42 <kohlmoja@gmx.de>
2016-07-24 15:35:06 +0200

    1a

---
+++ attacker.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jul 24 15:08:41 2016
+
+@author: Jan
+"""
+
+class attacker:
+    def __init__(self, name, speed, money, hp):
+        self.name = name
+        self.speed = speed
+        self.money = money
+        self.hp = hp
---
+++ field.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jul 24 15:21:45 2016
+
+@author: Jan
+"""
+
+class field:
+    def __init__():
+        pass
+    
+    def load()
