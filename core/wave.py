79e12ad37d1a42cbb04a7e673ee947147886e017
bluephoenix42 <kohlmoja@gmx.de>
2016-07-25 17:01:40 +0200

    spawn point in waves

--- core/wave.py
+++ core/wave.py
@@ -6,5 +6,6 @@ Created on Sun Jul 24 18:08:38 2016
 """
 import yaml
 class wave:
-	def __init__(self, attacker):
+	def __init__(self, attacker, spawn_point):
 		self.attacker = attacker #dict of the attacker (name:amount)
+		self.spawn_point = spawn_point
