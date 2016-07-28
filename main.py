1af56a3e971b6ce92892a4a0379515cea7509d58
jynn <uaggz@student.kit.edu>
2016-07-28 14:43:06 +0200

    added gui stuff to main

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ e ( events.load_level, "data/levels/1.yaml" )
 #create main window
 
 app = QtGui.QApplication(sys.argv)
-mw = main_window()
+mw = main_window(event_callback = e, gamestate = g)
 mw.show()
 mw.board.update_gamestate (g)
 sys.exit(app.exec_())
