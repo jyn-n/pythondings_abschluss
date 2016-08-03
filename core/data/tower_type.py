eeaa6008a8a82208ebec1cb4955458f55cbbf611
bluephoenix42 <kohlmoja@gmx.de>
2016-08-03 18:28:55 +0200

    attack_type

---
+++ core/data/tower_type.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jul 24 15:18:17 2016
+
+@author: Jan
+"""
+
+class tower:
+	def __init__(self, name, attack_range, attack_time, money, damage):
+		self.name = name
+		self.attack_range = attack_range
+		self.attack_time = attack_time
+		self.money = money
+		self.damage = damage
+        
