ae5cf95e39f1b3b890c077cc7e70c5fac685a2c9
bluephoenix42 <kohlmoja@gmx.de>
2016-08-03 18:41:08 +0200

    -

--- core/data/level.py
+++ core/data/level.py
@@ -7,7 +7,7 @@ Created on Sun Jul 24 18:06:22 2016
 
 class level:
 	def __init__(self, tower_type, field, waves, life, money):
-		self.tower_types = tower_type #list of towers filenames
+		self.tower_type = tower_type #list of towers filenames
 		self.field = field
 		self.waves = waves #dict of waves (time:wave)
 		self.life = life
